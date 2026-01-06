3eadac737aa13531ebb2d144bf15cf26f20916e4
cesarecarlo-mella <118001774+cesarecarlo-mella@users.noreply.github.com>
2026-01-06 18:00:24 +0000

    canonical basis almost done.
    Missing dim shift.
    Top sector.

---
+++ 2loop/graphs/topo1/shift_targets.m
@@ -0,0 +1,9 @@
+{
+{"topo1", 2, 3, MultiGraph[{Labeled[-1->1, "(p1,pp;1)"], Labeled[-2->1, "(-p1,pp;2)"], Labeled[2->2, "(k1,m2;3)"], Labeled[3->3, "(k2,m2;4)"]}]},
+{"topo1", 3, 7, MultiGraph[{Labeled[-1->2, "(p1,pp;1)"], Labeled[-2->2, "(-p1,pp;2)"], Labeled[3->1, "(k1,m2;3)"], Labeled[3->1, "(k2,m2;4)"], Labeled[1->3, "(k1+k2,m2;5)"]}]},
+{"topo1", 3, 11, MultiGraph[{Labeled[-1->2, "(p1,pp;1)"], Labeled[-2->1, "(-p1,pp;2)"], Labeled[2->1, "(k1,m2;3)"], Labeled[3->3, "(k2,m2;4)"], Labeled[1->2, "(k1-p1,m2;6)"]}]},
+{"topo1", 3, 14, MultiGraph[{Labeled[-1->2, "(p1,pp;1)"], Labeled[-2->1, "(-p1,pp;2)"], Labeled[1->2, "(k2,m2;4)"], Labeled[2->1, "(k1+k2,m2;5)"], Labeled[1->2, "(k1-p1,m2;6)"]}]},
+{"topo1", 4, 15, MultiGraph[{Labeled[-1->3, "(p1,pp;1)"], Labeled[-2->2, "(-p1,pp;2)"], Labeled[3->1, "(k1,m2;3)"], Labeled[2->1, "(k2,m2;4)"], Labeled[1->2, "(k1+k2,m2;5)"], Labeled[2->3, "(k1-p1,m2;6)"]}]},
+{"topo1", 4, 27, MultiGraph[{Labeled[-1->3, "(p1,pp;1)"], Labeled[-2->1, "(-p1,pp;2)"], Labeled[2->1, "(k1,m2;3)"], Labeled[2->3, "(k2,m2;4)"], Labeled[1->2, "(k1-p1,m2;6)"], Labeled[3->2, "(k2+p1,m2;7)"]}]},
+{"topo1", 5, 31, MultiGraph[{Labeled[-1->3, "(p1,pp;1)"], Labeled[-2->4, "(-p1,pp;2)"], Labeled[3->1, "(k1,m2;3)"], Labeled[4->1, "(k2,m2;4)"], Labeled[1->2, "(k1+k2,m2;5)"], Labeled[2->3, "(k1-p1,m2;6)"], Labeled[2->4, "(k2+p1,m2;7)"]}]}
+}
