cc3bc669c95b9d86d7ae607ec6e18a5df4619696
cesarecarlo-mella <118001774+cesarecarlo-mella@users.noreply.github.com>
2026-02-06 17:30:14 +0000

    rotation of banana
    progress towards canonical bassi

---
+++ yukawa/3loop/PERIODS/der2Omega0.m
@@ -0,0 +1,7 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Derivative[2][\[Omega]0][\[Rho]] -> 
+ -1/2*((-8 + \[Rho])*\[Omega]0[\[Rho]])/
+    (\[Rho]*(64 - 20*\[Rho] + \[Rho]^2)) - 
+  (2*(32 - 15*\[Rho] + \[Rho]^2)*Derivative[1][\[Omega]0][\[Rho]])/
+   (\[Rho]*(64 - 20*\[Rho] + \[Rho]^2)) + Derivative[1][\[Omega]0][\[Rho]]^2/
+   (2*\[Omega]0[\[Rho]])
---
+++ yukawa/3loop/PERIODS/der3Omega0.m
@@ -0,0 +1,7 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Derivative[3][\[Omega]0][\[Rho]] -> 
+ ((4 - \[Rho])*\[Omega]0[\[Rho]])/((-16 + \[Rho])*(-4 + \[Rho])*\[Rho]^2) + 
+  ((-64 + 68*\[Rho] - 7*\[Rho]^2)*Derivative[1][\[Omega]0][\[Rho]])/
+   ((-16 + \[Rho])*(-4 + \[Rho])*\[Rho]^2) - 
+  (6*(32 - 15*\[Rho] + \[Rho]^2)*Derivative[2][\[Omega]0][\[Rho]])/
+   ((-16 + \[Rho])*(-4 + \[Rho])*\[Rho])
