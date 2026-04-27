2606fe1a22097821f765f2e5e434beaec3259412
cesarecarlo-mella <118001774+cesarecarlo-mella@users.noreply.github.com>
2026-04-27 19:38:43 +0100

    fixes

---
+++ phi3/2_loop_eyeball_ancillary/PERIODS/d2omega0.m
@@ -0,0 +1,4 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{d2\[Omega]0 -> (-9*d\[Omega]0 + 20*d\[Omega]0*\[Rho] - 
+    3*d\[Omega]0*\[Rho]^2 + 3*\[Omega]0 - \[Rho]*\[Omega]0)/
+   ((-9 + \[Rho])*(-1 + \[Rho])*\[Rho])}
